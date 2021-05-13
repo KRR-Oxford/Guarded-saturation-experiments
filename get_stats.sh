e983afaa7637e06d024d23d12d9520a5f3a58431
Maxime Buron <maxron@clpc465.cs.ox.ac.uk>
2021-05-13 14:25:45 +0100

    fix get_stats script

--- get_stats.sh
+++ get_stats.sh
@@ -5,76 +5,122 @@ DIR=$1
 OUTPUT=$DIR/stats.csv
 CSV_SEP=","
 
-echo "NAME,NFTGD_NB,FTGD_NB,SUBSUMED,NEW_FTGD_NB,NEW_NFTGD_NB,NEW_OUTPUT_SIZE,OUTPUT_SIZE,TIME"> $OUTPUT
-for log in $DIR/*.log;
-do
 
 
-    # initialization of the values
-    NFTGD_NB="N/A"
-    FTGD_NB="N/A"
-    SUBSUMED="N/A"
-    NEW_NFTGD_NB="N/A"
-    NEW_FTGD_NB="N/A"
-    NEW_OUTPUT_SIZE="N/A"
-    OUTPUT_SIZE="N/A"
-    RUN_TIME_MS="N/A"
+if [[ $DIR =~ "kaon2" ]];
+then
+    echo "NAME,AXIOM_NB,OUTPUT_SIZE,TIME"> $OUTPUT
+    for log in $DIR/*.log;
+    do
+        # initialization of the values
+        AXIOM_NB="N/A"
+        OUTPUT_SIZE="N/A"
+        RUN_TIME_MS="N/A"
 
-    # get stats about input TGDs
-    TGD_STATS=`grep "INFOS: # initial TGDs:" $log || true`
-    if [[ -n $TGD_STATS ]];
-    then
-        SEPARATOR=" , "
-        NFTGD_NB=${TGD_STATS#*$SEPARATOR}
-        FTGD_NB=${TGD_STATS:23:$(( ${#TGD_STAT} - ${#NFTGD_NB} - ${#SEPARATOR} ))}
+        AXIOM_STATS=`grep "Initial axioms in the ontology: " $log || true`
+        AXIOM_NB=${AXIOM_STATS#*"ontology: "}
 
-        # get subsumed tgds
-        SUBSUMED_LINE=`grep "Subsumed elements :" $log || true`
+        OUTPUT_STATS=`grep "# rules: " $log || true`
+        OUTPUT_SIZE=${OUTPUT_STATS#*"rules: "}
 
-        if [[ -n $SUBSUMED_LINE ]];
+        # get run time in ms
+        RUN_TIME_LINE=`grep "total time :" $log`
+        RUN_TIME_SUFFIX=${RUN_TIME_LINE#*"time : "}
+        RUN_TIME=${RUN_TIME_LINE:$(( ${#RUN_TIME_LINE} - ${#RUN_TIME_SUFFIX} )):${#RUN_TIME_LINE}}
+        RUN_TIME_MS=${RUN_TIME%%" ms"*}
+
+        # Display the time out
+        IS_TIMEOUT=`grep "!!! TIME OUT !!!" $log || true`
+
+        if [[ -n $IS_TIMEOUT ]];
         then
-            SUBSUMED=${SUBSUMED_LINE#*"elements : "}
+            RUN_TIME_MS="TIMEOUT"
+        fi
 
-            # get derived TGDs
-            NEW_TGD_STATS=`grep "Derived full/non full TGDs: " $log || true`
-            NEW_FTGD_NB=${NEW_TGD_STATS#*$SEPARATOR}
-            NEW_NFTGD_NB=${NEW_TGD_STATS:35:$(( ${#NEW_TGD_STAT} - ${#NEW_FTGD_NB} - ${#SEPARATOR} ))}
+        # Display the error
+        IS_ERROR=`grep "!!! ERROR !!!" $log || true`
+
+        if [[ -n $IS_ERROR ]];
+        then
+            RUN_TIME_MS="ERROR"
+        fi
+        
+        FILE_NAME=`basename $log`
+        NAME=${FILE_NAME%".log"}
+        echo $NAME$CSV_SEP$AXIOM_NB$CSV_SEP$OUTPUT_SIZE$CSV_SEP$RUN_TIME_MS >> $OUTPUT
+    done
+else
+    echo "NAME,NFTGD_NB,FTGD_NB,SUBSUMED,NEW_FTGD_NB,NEW_NFTGD_NB,NEW_OUTPUT_SIZE,OUTPUT_SIZE,TIME"> $OUTPUT
+    for log in $DIR/*.log;
+    do
+        # initialization of the values
+        NFTGD_NB="N/A"
+        FTGD_NB="N/A"
+        SUBSUMED="N/A"
+        NEW_NFTGD_NB="N/A"
+        NEW_FTGD_NB="N/A"
+        NEW_OUTPUT_SIZE="N/A"
+        OUTPUT_SIZE="N/A"
+        RUN_TIME_MS="N/A"
+
+        # get stats about input TGDs
+        TGD_STATS=`grep "# initial TGDs:" $log || true`
+        if [[ -n $TGD_STATS ]];
+        then
+            TGD_STATS=${TGD_STATS#*"TGDs: "}
+            SEPARATOR=" , "
+            NFTGD_NB=${TGD_STATS#*$SEPARATOR}
+            FTGD_NB=${TGD_STATS:0:$(( ${#TGD_STAT} - ${#NFTGD_NB} - ${#SEPARATOR} ))}
 
             # get subsumed tgds
-            OUTPUT_LINE=`grep "Full TGD saturation: (" $log`
-            OUTPUT_SUFFIX=${OUTPUT_LINE#*"("}
-            OUTPUT_SIZE=${OUTPUT_SUFFIX%%" "*}
+            SUBSUMED_LINE=`grep "Subsumed elements :" $log || true`
 
-            # get ouptput full TGDs not contained in the input
-            NEW_OUTPUT_LINE=`grep "full TGDs not contained in the input: " $log`
-            NEW_OUTPUT_SIZE=${NEW_OUTPUT_LINE#*"input: "}
+            if [[ -n $SUBSUMED_LINE ]];
+            then
+                SUBSUMED=${SUBSUMED_LINE#*"elements : "}
 
-            # get run time in ms
-            RUN_TIME_LINE=`grep "total time :" $log`
-            RUN_TIME_SUFFIX=${RUN_TIME_LINE#*"time : "}
-            RUN_TIME=${RUN_TIME_LINE:$(( ${#RUN_TIME_LINE} - ${#RUN_TIME_SUFFIX} )):${#RUN_TIME_LINE}}
-            RUN_TIME_MS=${RUN_TIME%%" ms"*}
+                # get derived TGDs
+                NEW_TGD_STATS=`grep "Derived full/non full TGDs: " $log || true`
+                NEW_TGD_STATS=${NEW_TGD_STATS#*"TGDs: "}
+                NEW_NFTGD_NB=${NEW_TGD_STATS#*$SEPARATOR}
+                NEW_FTGD_NB=${NEW_TGD_STATS:0:$(( ${#NEW_TGD_STAT} - ${#NEW_NFTGD_NB} - ${#SEPARATOR} ))}
+
+                # get subsumed tgds
+                OUTPUT_LINE=`grep "Full TGD saturation: (" $log`
+                OUTPUT_SUFFIX=${OUTPUT_LINE#*"("}
+                OUTPUT_SIZE=${OUTPUT_SUFFIX%%" "*}
+
+                # get ouptput full TGDs not contained in the input
+                NEW_OUTPUT_LINE=`grep "full TGDs not contained in the input: " $log`
+                NEW_OUTPUT_SIZE=${NEW_OUTPUT_LINE#*"input: "}
+
+                # get run time in ms
+                RUN_TIME_LINE=`grep "total time :" $log`
+                RUN_TIME_SUFFIX=${RUN_TIME_LINE#*"time : "}
+                RUN_TIME=${RUN_TIME_LINE:$(( ${#RUN_TIME_LINE} - ${#RUN_TIME_SUFFIX} )):${#RUN_TIME_LINE}}
+                RUN_TIME_MS=${RUN_TIME%%" ms"*}
+            fi
         fi
-    fi
 
-    # Display the time out
-    IS_TIMEOUT=`grep "!!! TIME OUT !!!" $log || true`
+        # Display the time out
+        IS_TIMEOUT=`grep "!!! TIME OUT !!!" $log || true`
 
-    if [[ -n $IS_TIMEOUT ]];
-    then
-        RUN_TIME_MS="TIMEOUT"
-    fi
+        if [[ -n $IS_TIMEOUT ]];
+        then
+            RUN_TIME_MS="TIMEOUT"
+        fi
 
-    # Display the error
-    IS_ERROR=`grep "!!! ERROR !!!" $log || true`
+        # Display the error
+        IS_ERROR=`grep "!!! ERROR !!!" $log || true`
 
-    if [[ -n $IS_ERROR ]];
-    then
-        RUN_TIME_MS="ERROR"
-    fi
-    
-    FILE_NAME=`basename $log`
-    NAME=${FILE_NAME%".log"}
-    echo $NAME$CSV_SEP$NFTGD_NB$CSV_SEP$FTGD_NB$CSV_SEP$SUBSUMED$CSV_SEP$NEW_NFTGD_NB$CSV_SEP$NEW_FTGD_NB$CSV_SEP$NEW_OUTPUT_SIZE$CSV_SEP$OUTPUT_SIZE$CSV_SEP$RUN_TIME_MS >> $OUTPUT
+        if [[ -n $IS_ERROR ]];
+        then
+            RUN_TIME_MS="ERROR"
+        fi
+        
+        FILE_NAME=`basename $log`
+        NAME=${FILE_NAME%".log"}
+        echo $NAME$CSV_SEP$NFTGD_NB$CSV_SEP$FTGD_NB$CSV_SEP$SUBSUMED$CSV_SEP$NEW_FTGD_NB$CSV_SEP$NEW_NFTGD_NB$CSV_SEP$NEW_OUTPUT_SIZE$CSV_SEP$OUTPUT_SIZE$CSV_SEP$RUN_TIME_MS >> $OUTPUT
 
-done
+    done
+fi
