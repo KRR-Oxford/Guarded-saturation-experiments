fa4d470d5e99ad749fddcc5894b46b8eba772fbf
Maxime Buron <mburon@mburon-pc.saclay.inria.fr>
2021-05-19 18:47:06 +0100

    deploy script

---
+++ results/ISG-guarded_kaon2/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+scp -r plots maxron@linux.cs.ox.ac.uk:/fs/website/people/maxime.buron/blog/gsat-exp/
+scp analyze.html maxron@linux.cs.ox.ac.uk:/fs/website/people/maxime.buron/blog/gsat-exp/index.html
+scp style.css maxron@linux.cs.ox.ac.uk:/fs/website/people/maxime.buron/blog/gsat-exp/
