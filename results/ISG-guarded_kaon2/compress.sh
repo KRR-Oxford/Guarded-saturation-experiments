3a797553f13db845b8aa57460e72392bcbdad586
Maxime Buron <maxime.buron@cs.ox.ac.uk>
2022-01-25 17:08:44 +0000

    results saving

--- results/ISG-guarded_kaon2/compress.sh
+++ results/ISG-guarded_kaon2/compress.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-for dir in 2021*5; do
+for dir in 2021-12*; do
     echo $dir;
     cd $dir;
-    if [ ! -f $dir.zip ];
+    if [ ! -f $dir.zip ] && [ -f "00001.rul" ];
     then
         zip $dir.zip *.rul
     fi
